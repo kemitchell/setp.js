edb12581bbb2546e797734f13bacdb5c0144241b
Kyle E. Mitchell <kyle@kemitchell.com>
2016-08-23 22:33:17 -0700

    Use JavaScript Standard Style

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 var setp = require('setp')
 var assert = require('assert')
 
-var firstExample = { }
-setp(firstExample, [ 'a', 'b', 'c' ], 'value')
-assert.deepEqual(firstExample, { a: { b: { c: 'value' } } })
+var firstExample = {}
+setp(firstExample, ['a', 'b', 'c'], 'value')
+assert.deepEqual(firstExample, {a: {b: {c: 'value'}}})
 
-assert.throws(
-  function() {
-    setp({ a: 'string' }, [ 'a', 'b', 'c' ], 'value') })
+assert.throws(function() {
+  setp({ a: 'string' }, ['a', 'b', 'c'], 'value')
+})
 ```
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-function setp(object, keys, value) {
+module.exports = function setp (object, keys, value) {
   var length = keys.length
   if (length !== 0) {
     var firstKey = keys[0]
     if (length === 1) {
-      object[firstKey] = value }
-    else {
-      var nextKey = keys[1]
+      object[firstKey] = value
+    } else {
       if (!(firstKey in object)) {
-        object[firstKey] = { } }
-      setp(object[firstKey], keys.slice(1), value) } } }
-
-module.exports = setp
+        object[firstKey] = {}
+      }
+      setp(object[firstKey], keys.slice(1), value)
+    }
+  }
+}
